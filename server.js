53e5e4035b35b7a7766431c8d73ca49c13e29e90
endriu17 <aolaszewski@gmail.com>
2018-11-05 21:27:27 +0100

    add files to branch

--- server.js
+++ server.js
@@ -1,23 +1,63 @@
 var express = require('express');
 var app = express();
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var config = require('./config/config');
+var googleProfile = {};
 
 app.set('view engine', 'pug');
 app.set('views','./views');
 
-app.get('/login-template', function(req, res) {
-  res.render('login-template', {
-      name: "Website for login using Google",
-      url: "/auth/google"
-  });
-})
-app.get('/auth/google', function(req, res) {
-  res.send("Congratulations! You are logged in!");
-  console.log('Login completed successfully!');
-})
-
 app.use(express.static(__dirname + '/public'));
 
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+passport.use(new GoogleStrategy({
+  clientID: config.GOOGLE_CLIENT_ID,
+  clientSecret:config.GOOGLE_CLIENT_SECRET,
+  callbackURL: config.CALLBACK_URL
+},
+function(accessToken, refreshToken, profile, cb) {
+  googleProfile = {
+      id: profile.id,
+      displayName: profile.displayName
+  };
+  cb(null, profile);
+}
+));
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+app.use(passport.initialize());
+app.use(passport.session());
+
+//app routes
+app.get('/', function(req, res){
+  res.render('index', { user: req.user });
+});
+
+app.get('/logged', function(req, res){
+  res.render('logged', { user: googleProfile });
+});
+
+//Passport routes
+app.get('/auth/google',
+passport.authenticate('google', {
+scope : ['profile', 'email']
+}));
+app.get('/auth/google/callback',
+  passport.authenticate('google', {
+      successRedirect : '/logged',
+      failureRedirect: '/'
+  }));
+
 app.listen(3000);
+
 app.use(function (req, res, next){
   res.status(404).send("We're sorry, but we couldn't find your request.")
   next();
